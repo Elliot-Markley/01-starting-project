d387e24f0bc292e91880aeb2a1d510a4095dac6c
Elliot Markley <Elliotmarkley.dev@gmail.com>
2024-01-04 20:14:35 -0500

    Split app up

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,14 @@
-import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
-import TabButton from "./components/TabButton";
-import { useState } from "react";
+import Examples from "./components/Examples";
 
 function App() {
-  const [tabContent, setTabContent] = useState();
-
-  function handelSelect(selectedButton) {
-    setTabContent(selectedButton);
-  }
-
-  let selectedContent = <p>Please select a topic.</p>;
-
-  if (tabContent) {
-    selectedContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[tabContent].title}</h3>
-        <p>{EXAMPLES[tabContent].description}</p>
-        <pre>
-          <code>{EXAMPLES[tabContent].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
   return (
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcepts
-                key={conceptItem.title}
-                {...conceptItem}
-              />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={tabContent === "components"}
-              onSelect={() => handelSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={tabContent === "jsx"}
-              onSelect={() => handelSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={tabContent === "props"}
-              onSelect={() => handelSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={tabContent === "state"}
-              onSelect={() => handelSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {selectedContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
         <h2>Time to get started!</h2>
       </main>
     </>
---
+++ src/components/Examples.jsx
@@ -0,0 +1,58 @@
+import TabButton from "./TabButton";
+import { useState } from "react";
+import { EXAMPLES } from "../data";
+
+export default function Examples() {
+  const [tabContent, setTabContent] = useState();
+
+  function handelSelect(selectedButton) {
+    setTabContent(selectedButton);
+  }
+
+  let selectedContent = <p>Please select a topic.</p>;
+
+  if (tabContent) {
+    selectedContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[tabContent].title}</h3>
+        <p>{EXAMPLES[tabContent].description}</p>
+        <pre>
+          <code>{EXAMPLES[tabContent].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton
+          isSelected={tabContent === "components"}
+          onSelect={() => handelSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={tabContent === "jsx"}
+          onSelect={() => handelSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={tabContent === "props"}
+          onSelect={() => handelSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={tabContent === "state"}
+          onSelect={() => handelSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {selectedContent}
+    </section>
+  );
+}
